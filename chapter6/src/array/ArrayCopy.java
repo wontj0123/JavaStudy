bc7fc5413ebbe31ac3c756791595f917e4741a9a
wontj0123 <xowns150@gmail.com>
2021-01-29 17:58:36 +0900

    자바 객체 공부

---
+++ chapter6/src/array/ArrayCopy.java
@@ -0,0 +1,8 @@
+package array;
+
+public class ArrayCopy {
+	public static void main(String[] args) {
+		
+		
+		}
+	}
---
+++ chapter6/src/array/arrayTest.java
@@ -0,0 +1,13 @@
+package array;
+
+public class arrayTest {
+	public static void main(String[] args) {
+		
+		int[] arr = new int[10];
+		
+		for (int i =0; i<=arr.length; i++) {
+			System.out.println(arr[i]);
+			}
+		
+	}
+}

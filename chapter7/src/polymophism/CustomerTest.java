9ba851c1240005b01037e21e16f021b8dd6c4b43
wontj0123 <xowns150@gmail.com>
2021-02-16 18:44:09 +0900

    customerTest 수정

--- chapter7/src/polymophism/CustomerTest.java
+++ chapter7/src/polymophism/CustomerTest.java
@@ -3,12 +3,6 @@ package polymophism;
 public class CustomerTest {
 	public static void main(String[] args) {
 		
-		/*
-		 * if(grade == GOLD) {
-		 * 
-		 * }else if(grade == SILVER) {
-		 * 
-		 * }
-		 */
+	
 	}
 }

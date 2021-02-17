f82a39eb096edf4fe69a4f7120024d0431eb3bec
wontj0123 <xowns150@gmail.com>
2021-02-17 14:50:48 +0900

    instanceof, 업캐스팅,다운캐스팅

--- chapter7/src/polymophism/AnimalTest.java
+++ chapter7/src/polymophism/AnimalTest.java
@@ -11,17 +11,26 @@ class Human extends Animal{
 	public void move() {
 		System.out.println("사람이 두발로 걷습니다.");
 	}
+	public void readBooks() {
+		System.out.println("사람이 책을 읽습니다.");
+	}
 }
 
 class Tiger extends Animal{
 	public void move() {
 		System.out.println("호랑이가 네 발로 뜁니다.");
 	}
+	public void huntiong() {
+		System.out.println("호랑이가 사냥을 합니다.");
+	}
 }
 
 class Eagle extends Animal{
 	public void move() {
 		System.out.println("독수리가 하늘을 날아갑니다.");
+	}
+	public void flying() {
+		System.out.println("독수리가 날개를 쭉 펴고 멀리 날아갑니다.");
 	}
 }
 public class AnimalTest {
@@ -30,8 +39,18 @@ public class AnimalTest {
 		Animal tAnimal =new Tiger();
 		Animal eAnimal =new Eagle();
 		
-		AnimalTest test = new AnimalTest();
 		/*
+		 * Human human =(Human)hAnimal; 
+		 * human.readBooks();
+		 */
+		
+		if(hAnimal instanceof Human) {
+			Human human = (Human)hAnimal;
+			human.readBooks();
+		}
+		
+		/*
+		 * AnimalTest test = new AnimalTest();
 		 * test.moveAnimal(hAnimal); 
 		 * test.moveAnimal(tAnimal); 
 		 * test.moveAnimal(eAnimal);
@@ -44,12 +63,35 @@ public class AnimalTest {
 		  animalList.add(tAnimal); 
 		  animalList.add(eAnimal);
 		 
+		  AnimalTest test = new AnimalTest();
+		  test.testDownCasting(animalList);
 		 
 		
 		for(Animal animal : animalList) {
 			animal.move();
 		}
 		
+	}
+	public void testDownCasting(ArrayList<Animal> list) {
+		for(int i =0; i<list.size();i++) {
+			Animal animal = list.get(i);
+			
+			if(animal instanceof Human) {
+				Human human = (Human)animal;
+				human.readBooks();
+			}
+			else if(animal instanceof Tiger) {
+				Tiger tiger = (Tiger)animal;
+				tiger.huntiong();
+			}
+			else if(animal instanceof Eagle) {
+				Eagle eagle = (Eagle)animal;
+				eagle.flying();
+			}
+			else {
+				System.out.println("error");
+			}
+		}
 	}
 	
 	public void moveAnimal(Animal animal) {

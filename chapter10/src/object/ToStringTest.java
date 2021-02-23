bb4436d47b426aa754e6b70b24b9bcf3888a5b98
wontj0123 <xowns150@gmail.com>
2021-02-23 15:23:44 +0900

    object class clone() 사용

--- chapter10/src/object/ToStringTest.java
+++ chapter10/src/object/ToStringTest.java
@@ -1,29 +1,45 @@
 package object;
 
-class Book{
+class Book implements Cloneable{
 	String title;
 	String author;
 	
-	public Book(String title, String author ) {
+	public Book(String title, String author) {
 		this.title = title;
 		this.author = author;
 	}
 
 	@Override
 	public String toString() {
-		return author + ","+title;
+		return author + "," +title;
+	}
+
+	@Override
+	protected Object clone() throws CloneNotSupportedException {
+		
+		return super.clone();
+	}
+
+	@Override
+	protected void finalize() throws Throwable {
+		// TODO Auto-generated method stub
+		super.finalize();
 	}
 	
 }
 
 public class ToStringTest {
-	public static void main(String[] args) {
-		
+
+	public static void main(String[] args) throws CloneNotSupportedException {
+
 		Book book = new Book("토지", "박경리");
-		System.out.println(book);
-		System.out.println(book.toString());
 		
-		String str = new String("토지");
-		System.out.println(str);
+		System.out.println(book);
+		
+		Book book2 = (Book)book.clone();
+		System.out.println(book2);
+
+	
 	}
+
 }
